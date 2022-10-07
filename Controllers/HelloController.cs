fcb8cc7b7f691d4618e88d307e33406c4223b0bc
Emreman5 <emre15087@gmail.com>
2022-10-07 23:45:01 +0100

    init

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RpiServerAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class HelloController : Controller
+{
+    [HttpGet("hello")]
+    public IActionResult Hello()
+    {
+        return Ok("Batuş");
+    }
+    [HttpGet("GetLocation")]
+    public IActionResult GetLocation()
+    {
+        return Ok("X : 100, Y : 50");
+    }
+}
