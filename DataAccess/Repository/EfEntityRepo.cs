3cb40f1e3fd9562c44ce8280bc5e241cca4bdca8
Emreman5 <emre15087@gmail.com>
2022-10-12 16:56:09 +0300

    Repository implementation.

--- DataAccess/Repository/EfEntityRepo.cs
+++ DataAccess/Repository/EfEntityRepo.cs
@@ -1,1 +1,66 @@
-﻿
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace RpiServerAPI.DataAccess.Repository;
+
+public class EfEntityRepo<TEntity, TContext> : IEntityRepo<TEntity>
+    where TEntity : class, new()
+    where TContext : DbContext, new()
+
+{
+    public void Add(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+    }
+
+    public void Delete(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
+
+    public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+    {
+        using (TContext context = new TContext())
+        {
+            return filter == null ? context.Set<TEntity>().ToList() :
+                context.Set<TEntity>().Where(filter).ToList(); 
+        }
+    }
+    public TEntity Get(Expression<Func<TEntity, bool>> filter)
+    {
+        using (var context = new TContext())
+        {
+            return context.Set<TEntity>().SingleOrDefault(filter);
+        }
+    }
+
+    public TEntity GetById(Expression<Func<TEntity,bool>> filter)
+    {
+        using (TContext context = new TContext())
+        {
+            return context.Set<TEntity>().SingleOrDefault(filter);
+        }
+    }
+
+   
+
+    public void Update(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
--- DataAccess/Repository/IEntityRepo.cs
+++ DataAccess/Repository/IEntityRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace RpiServerAPI.DataAccess.Repository;
+﻿using System.Linq.Expressions;
 
-public interface IEntityRepo
+namespace RpiServerAPI.DataAccess.Repository;
+
+public interface IEntityRepo<T> where T:class,new()
 {
-    
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    List<T> GetAll(Expression<Func<T,bool>> filter=null);
+    T GetById(Expression<Func<T, bool>> filter);
+    T Get(Expression<Func<T, bool>> filter);
+
 }
