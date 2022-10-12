68ccd001d1a7a0359ca03f6af2dacbafaa1c72ec
Emreman5 <emre15087@gmail.com>
2022-10-12 16:53:05 +0300

    Context Created

--- DataAccess/DbContext.cs
+++ DataAccess/DbContext.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using RpiServerAPI.Models;
+
+namespace RpiServerAPI.DataAccess;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+    public DbSet<MapData> Maps { get; set; }
+    public DbSet<Beacon> Beacons { get; set; }
+
+    public DataContext(IConfiguration configuration, DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+    }
+
+    public DataContext()
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Beacon[] beaconSeeds = new[]
+        {
+            new Beacon()
+            {
+                Id = 1,
+                Uuid = "dadadada",
+                LocationX = 200,
+                LocationY = 300,
+                MapId = 3,
+                Status = true,
+                
+            }
+        };
+        modelBuilder.Entity<Beacon>().HasData(beaconSeeds);
+    }
+}
+   
